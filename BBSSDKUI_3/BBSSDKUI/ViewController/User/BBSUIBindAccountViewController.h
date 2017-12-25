46e10b8cf72c5e392eb7acd074a1fbc947d3b0b8
chuxiao <chuxiao@chuxiaodeMac-mini.local>
2017-12-25 14:50:39 +0800

    2.2

---
+++ BBSSDKUI_3/BBSSDKUI/ViewController/User/BBSUIBindAccountViewController.h
@@ -0,0 +1,15 @@
+//
+//  BBSUIBindAccountViewController.h
+//  BBSSDKUI
+//
+//  Created by chuxiao on 2017/11/22.
+//  Copyright © 2017年 MOB. All rights reserved.
+//
+
+#import "BBSUIBaseUserViewController.h"
+
+@interface BBSUIBindAccountViewController : BBSUIBaseUserViewController
+
+@property (nonatomic, strong) NSDictionary *params;
+
+@end
---
+++ BBSSDKUI_WF/BBSSDKUI/ViewController/Common/UITextField+ExtentRange.h
@@ -0,0 +1,16 @@
+//
+//  UITextField+ExtentRange.h
+//  BBSSDKUI
+//
+//  Created by chuxiao on 2017/10/25.
+//  Copyright © 2017年 MOB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UITextField ()
+
+- (NSRange) selectedRange;
+- (void) setSelectedRange:(NSRange) range;
+
+@end
